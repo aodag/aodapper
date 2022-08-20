26a05bfaca35bb963c7784293516618d5ca36a0a
Atsushi Odagiri <aodagx@gmail.com>
2022-08-20 12:22:20 +0900

    add dataclass to example

--- examples/pg.py
+++ examples/pg.py
@@ -1,5 +1,15 @@
 import asyncio
+import dataclasses
+import uuid
 import asyncpg
+
+
+@dataclasses.dataclass
+class Dog:
+    age: int
+    id: uuid.UUID
+    name: str
+    weight: int
 
 
 async def run():
@@ -9,8 +19,25 @@ async def run():
         database="testdb",
         host="127.0.0.1",
     )
-    values = await conn.fetch("SELECT CURRENT_TIMESTAMP")
-    print(values)
+    await conn.execute(
+        "CREATE TABLE IF NOT EXISTS dogs (id uuid PRIMARY KEY, age int, name text, weight int )"
+    )
+    dog = Dog(age=10, id=uuid.uuid4(), name="aodog", weight=5)
+    await conn.execute(
+        "INSERT INTO dogs (id, age, name, weight) VALUES ($1, $2, $3, $4)",
+        dog.id,
+        dog.age,
+        dog.name,
+        dog.weight,
+    )
+    values = await conn.fetch("SELECT * FROM dogs")
+    for r in values:
+        d = Dog(**dict(r.items()))
+        print(d)
+    await conn.execute(
+        "DELETE FROM dogs WHERE id = $1",
+        dog.id,
+    )
     await conn.close()
 
 
